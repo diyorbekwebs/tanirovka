6d3b02814023112b6917747ae46cc3f2875fbc4e
Getteruz <uzgetter@gmail.com>
2024-09-16 13:49:55 +0500

    f

--- src/assets/image/img.js
+++ src/assets/image/img.js
@@ -1,4 +1,7 @@
 import Logo from "./Logo.jpg";
 import Delli from "./hero.jpg";
-import Call from './phone-solid.svg'
-export { Logo, Delli ,Call};
+import Call from "./phone-solid.svg";
+import Instagram from "./instagram.svg";
+import Telegram from "./telegram.svg";
+import Facebook from "./facebook.svg";
+export { Logo, Delli, Call, Instagram, Telegram, Facebook };
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,3 +1,4 @@
+import { Facebook, Instagram, Telegram } from "../../assets/image/img";
 
 export default function Header() {
   return (
@@ -12,9 +13,15 @@ export default function Header() {
           </ul>
           <div className="logo text-[#FF8A00] text-[30px] ">Logo</div>
           <ul className="icons flex gap-[30px]">
-            <li className="icon">tg</li>
-            <li className="icon">face</li>
-            <li className="icon">ins</li>
+            <li className="icon">
+              <img className="w-[20px] text-[#ffff]" src={Telegram} alt="" />
+            </li>
+            <li className="icon">
+              <img className="w-[20px] text-[#ffff]" src={Facebook} alt="" />
+            </li>
+            <li className="icon">
+              <img className="w-[20px] text-[#ffff]" src={Instagram} alt="" />
+            </li>
           </ul>
         </nav>
       </div>
