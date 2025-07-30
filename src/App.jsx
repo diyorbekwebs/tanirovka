4fe3f7e1ed77da002b87d2b9943d177607ad4abb
Diyorbek Abdullayev <diyorbekwebs@Diyorbeks-MacBook-Pro.local>
2025-07-30 23:16:50 +0500

    vercel

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,20 @@ import Precut from "./pages/precut/Precut";
 import Window from "./pages/window/Window";
 import Paint from "./pages/paint/Paint";
 import ScrollToTop from "./scrolltop";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <>
+     <Helmet>
+        <title>Tanirovka.uz – Avtomobil oynalarini qoraytirish xizmati</title>
+        <meta name="description" content="Toshkentdagi eng yaxshi avtomobil oynalarini qoraytirish xizmati." />
+      </Helmet>
       <ScrollToTop />
       <Cursor size={50} color="#0073ff" hollow={true} />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="fitting" element={<Fitting />} />
           <Route path="precut" element={<Precut />} />
           <Route path="window" element={<Window />} />
           <Route path="paint" element={<Paint />} />
